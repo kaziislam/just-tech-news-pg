1fefd8d1d474ea8c04cdd3e89086d0d3b4ec8703
Kazi Islam <kazichaska@gmail.com>
2022-12-16 22:39:42 -0600

    changed frommysql to postgres config

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,12 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create connection to our database, pass in your MySQL information for username and password
+// create connection to our database, pass in your Postgresql information for username and password
 // const sequelize = new Sequelize('just_tech_news_db', 'username', 'password', {
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.database, process.env.username, process.env.password, {
     host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
+    dialect: 'postgres',
+    port: 5432
 });
 
 module.exports = sequelize;
